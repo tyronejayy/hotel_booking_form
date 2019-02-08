a87cb7a13ef63f6dee144abbd9e005511803f467
tyronejayy <tyronejoubert22@gmail.com>
2019-02-08 16:07:36 +0200

    created base files and added some content

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>hotel-booking-form</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+<!-- -------------------------------------------------- -->
+<!-- -------------------------------------------------- -->
+<!-- -------------------------------------------------- -->
+
+<div class="form-style-3">
+    <form style="background:transparent;">
+        <fieldset><legend>Booking ?? Get looking!</legend>
+
+            <h3>Hotel-Booking-Form</h3>
+            
+            <label for="field1">
+                <span>Name <span class="required">*</span></span><input type="text" class="input-field" name="field1" value="" />
+            </label>
+
+            <label for="field2">
+                <span>Password <span class="required">*</span></span><input type="password" class="input-field" name="field2" value="" />
+            </label>
+
+            <label for="field3">
+                <span>Confirm Password <span class="required">*</span></span><input type="password" class="input-field" name="field3" value="" />
+            </label>
+
+            <!-- <label for="field4"><span>Subject</span><select name="field4" class="select-field">
+            <option value="Appointment">Appointment</option>
+            <option value="Interview">Interview</option>
+            <option value="Regarding a post">Regarding a post</option>
+            </select></label> -->
+            <p>
+                already have an account? <a href="login.php">Login</a>
+            </p>
+        </fieldset>
+        <!-- <fieldset><legend>Message</legend>
+        <label for="field6"><span>Message <span class="required">*</span></span><textarea name="field6" class="textarea-field"></textarea></label>
+        <label><span> </span><input type="submit" value="Submit" /></label>
+        </fieldset> -->
+    </form>
+</div>
+  
+<h1>
+    Hotel for your honeymoon, vacation or even just a weekend getaway ?? You've come to the right place! We guarantee an unforgettable stay.
+</h1>
+<div class="main-container">
+    <div>
+        <img src="img/fancy-hotel.jpg"; width="100%"; height="100%";>
+    </div>
+    <div>
+        <img src="img/fancy-hotel.jpg"; width="100%"; height="100%";>
+    </div>
+    <div>
+        <img src="img/fancy-hotel.jpg"; width="100%"; height="100%";>
+    </div>
+</div>
+    
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/styles.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+<div class="form-style-3">
+<form style="background:transparent;">
+<fieldset><legend>Login</legend>
+<h3>Hotel-Booking-Form</h3> <br>
+    <label for="field1"><span>Name <span class="required">
+        *</span></span><input type="text" class="input-field" name="field1" value="" />
+    </label> <br>
+
+    <label for="field2"><span>Email <span class="required">
+        *</span></span><input type="email" class="input-field" name="field2" value="" />
+    </label><br>
+    <p>
+        Don't have an account? <a href="index.php">Sign-up</a>
+    </p>
+</fieldset>
+
+</form>
+</div>
+    
+</body>
+</html>
+
